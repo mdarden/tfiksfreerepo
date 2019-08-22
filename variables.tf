f4bd84e431aa85b1686e8a0fcd6a68dbe3176b73
mdarden <mdarden@users.noreply.github.com>
2019-08-22 14:14:28 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "ibmcloud_api_key" {default = "oImKWxZqcPUIr7PvPotDhircM-4ozY50uBBXdDmZG5-A"}
 
-variable "region" {}
+variable "region" {default = "us-south"}
 
 variable "cluster_name" {
   default = "clusterfree"
