c0a75b3d5a17275073614e36746afd6c071ef70d
mdarden <mdarden@users.noreply.github.com>
2019-08-22 14:26:07 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
 resource "ibm_container_cluster" "cluster" {
   name              = "${var.cluster_name}${random_id.name.hex}"
   datacenter        = "${var.datacenter}"
+  hardware        = "${var.hardware}"
 }
 
 resource "random_id" "name" {
