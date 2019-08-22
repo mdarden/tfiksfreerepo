07d39b8ab209e54a5d66cdbe3256725c00012278
mdarden <mdarden@users.noreply.github.com>
2019-08-22 14:11:01 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,7 @@
 variable "ibmcloud_api_key" {}
 
 variable "region" {}
-variable "machine_type" {}
-variable "hardware" {}
-
-variable "datacenter" {}
-
-variable "private_vlan_id" {}
-
-variable "public_vlan_id" {}
 
 variable "cluster_name" {
-  default = "cluster"
+  default = "clusterfree"
 }
